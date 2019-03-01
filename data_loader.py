2686cb1b1c12e9e3f098e37e20b66c2e3bcc5a92
Wu Yihan <yihan.wu@tum.de>
2019-03-02 00:18:39 +0100

    rmfiles

--- data_loader.py
+++ data_loader.py
@@ -19,76 +19,6 @@ import torch.utils.data
 from torch.autograd import Variable
 
 
-class CoevolutionData(torch.utils.data.Dataset):
-    """
-    Dataset class for co-evolution matrices, to load one memmap file at a time.
-    """
-    def __init__(self, x, y, coev_memmap_dir, coev_ext, pt_seq_dir):
-        """
-            :param x: L
-            :param y: List of numpy arrays containing ground truth data
-            """
-        self.x = x
-        self.y = y
-        self.channels = 441
-        self.coev_memmap_dir = coev_memmap_dir
-        self.coev_ext = coev_ext
-        self.pt_seq_dir = pt_seq_dir
-
-    def __len__(self):
-        return len(self.y)
-
-    def __getitem__(self, item):
-        # print("Fetching entry {} from data loader..".format(item))
-
-        protein_name = self.x[item]
-        label = self.y[item]
-
-        # find the memmap path for the coevolution data of a particular protein
-        coev_memmap_path = glob.glob(os.path.join(self.coev_memmap_dir, protein_name+self.coev_ext+'*.memmap'))[0]
-        # get length of protein from filename
-
-        name_parts = coev_memmap_path.split('_')
-        prot_len = int(name_parts[-5])
-
-        # New data computed has shape (L*L*C)
-        loaded_memmap = np.memmap(coev_memmap_path, dtype=np.float32, mode='r', shape=(prot_len, prot_len, self.channels))
-
-        coev_map = np.empty((prot_len, prot_len, self.channels), dtype=np.float32)
-        coev_map[:] = loaded_memmap[:]
-        del loaded_memmap
-        #delete YW
-        # # added YW build protein sequences feature
-        # pt_seq_path = glob.glob(os.path.join(self.pt_seq_dir, protein_name + '*.fa'))[0]
-        # loaded_pt = read_and_convert_ptseq(pt_seq_path)
-        # print("get",np.shape(loaded_pt))
-        # coev_map = np.concatenate((coev_map,loaded_pt),axis=2)
-        # # end added
-        # del loaded_pt
-        # end delete
-        # transpose because neural network expects (C,N,N)
-
-        coev_map = np.transpose(coev_map, (2, 1, 0))
-        return coev_map, label
-#added YW build protein sequences feature
-def read_and_convert_ptseq(pt_seq_path):
-    allSeqs = []
-    convert_ptseq = []
-    ptseq = list((open(pt_seq_path).read()).split("\n"))
-    for i in range(len(ptseq) - 1):
-        allSeqs += ptseq[i + 1]
-    table = {'A': 1, 'C': 2, 'D': 3, 'E': 4, 'F': 5, 'G': 6, 'H': 7, 'I': 8, 'K': 9, 'L': 10, 'M': 11, 'N': 12, 'P': 13,
-             'Q': 14, 'R': 15, 'S': 16, 'T': 17, 'V': 18, 'W': 19, 'Y': 20, 'X':21, 'U':22}
-    for x in range(0, len(allSeqs), 1):
-        convert_ptseq.append(table[allSeqs[x]])
-    convert_ptseq = np.array(convert_ptseq)
-    loaded_pt = np.zeros((len(convert_ptseq), len(convert_ptseq), 22),dtype=np.float32)
-    loaded_pt[np.arange(len(convert_ptseq)), np.arange(len(convert_ptseq)), convert_ptseq - 1] = 1
-    return loaded_pt
-#end(added)
-
-
-
 def get_protein_names(memmap_dir):
     """
     Get proteins identifiers from file names.
@@ -155,7 +85,7 @@ def calculate_label_weights(unique_values, function_count_dict, num_samples):
     return function_weights
 
 
-def prepare_data(memmap_path, train_val_ratio, num_loaded_proteins, max_protein_len, min_protein_len, func_threshold, pt_seq_dir, verbose=True):
+def prepare_data(memmap_path, train_val_ratio, num_loaded_proteins):
     """
     Prepare data
     :param memmap_path: string
@@ -267,8 +197,6 @@ def prepare_data(memmap_path, train_val_ratio, num_loaded_proteins, max_protein_
 
     # Splitting datasets
     x_train, y_train, x_val, y_val = train_val_split(x, y, train_val_ratio, num_proteins)
-    train_data = CoevolutionData(x_train, y_train, coev_memmap_dir, coev_ext, pt_seq_dir)
-    val_data = CoevolutionData(x_val, y_val, coev_memmap_dir, coev_ext, pt_seq_dir)
 
     # Preparing index lists for the three main ontologies
     idxMF = []
@@ -290,100 +218,10 @@ def prepare_data(memmap_path, train_val_ratio, num_loaded_proteins, max_protein_
         x_val,
         y_train,
         y_val,
-        train_data,
-        val_data,
-        function_weights,
         num_classes,
         unique_go_values,
         idxMF,
         idxBP,
         idxCC
     )
-
-def prepare_test_data(memmap_path,protein_names,max_protein_len, min_protein_len,func_threshold,pt_seq_dir):
-    coev_subdir = 'ccmpred/J_and_h/'
-    go_terms_subdir = 'GOterms'
-    coev_ext = '_J_'
-    goterms_ext = '.GOterms.memmap'
-    coev_memmap_dir = os.path.join(memmap_path, coev_subdir)
-    go_terms_memmap_dir = os.path.join(memmap_path, go_terms_subdir)
-
-    go_ontology_filename = os.path.join(memmap_path, 'Scripts/hierarchical_classification_GO_terms/go_db/term.txt')
-    main_ontologies_per_term_dict = {}
-    ontology_labels = {'molecular_function': 'F', 'biological_process': 'P', 'cellular_component': 'C'}
-    with open(go_ontology_filename) as f:
-        for line in f:
-            (termID, description, ontology, GOID) = line.split(',\'')
-            if GOID[:-2][:3] == 'GO:' and ontology[:-1] != '':
-                main_ontologies_per_term_dict[int(GOID[:-2][3:])] = ontology_labels[ontology[:-1]]
-
-    x_test = []  # List of proteins, where both coevolution and function memmap files exist (previously protein_names_existing)
-    funcs_instances_counter = Counter()  # Counter for annotations per function over all loaded proteins
-    funcs_dict = defaultdict(list)
-    num_proteins = 0
-    while num_proteins < len(protein_names):
-        protein_name = protein_names[num_proteins]
-        func_memmap_path = os.path.join(go_terms_memmap_dir, protein_name + goterms_ext)
-        func_memmap_path = glob.glob(func_memmap_path)
-        if not (func_memmap_path):  # (coev_memmap_path and func_memmap_path):
-            continue
-        # Process memmap file of protein annotations, build a counter with number of annotations for each function
-        x_test.append(protein_name)
-        func_memmap_path = func_memmap_path[0]
-        func_memmap = np.memmap(func_memmap_path, dtype=np.int32, mode='r')
-        for func in func_memmap:
-            # if func in list(main_ontologies_per_term_dict.keys()):
-            #    if (main_ontologies_per_term_dict[func] == 'F'):
-            funcs_instances_counter[func] += 1
-            funcs_dict[protein_name].append(func)
-        del func_memmap
-
-        # To be removed. Number of proteins to load, ideally load all ~40K proteins
-        num_proteins += 1
-        if num_proteins % 100 == 0:
-            print("Loaded {} proteins".format(num_proteins))
-    print("Finished loading {} proteins total".format(num_proteins))
-
-    # Remove rare functions - we don't try to predict them
-    print("Total unique classes: {}".format(len(funcs_instances_counter)))
-    funcs_instances_counter = filter_functions(funcs_instances_counter, func_threshold)
-    print("Total unique classes after filtering: {}".format(len(funcs_instances_counter)))
-
-    # Sort GO terms in ascending order
-    unique_go_values = sorted(funcs_instances_counter.keys())
-    num_classes = len(unique_go_values)
-    # Compiling ground truth vectors
-
-    y_test = []
-    for protein_name in x_test:
-        np_ground_truth = np.zeros((num_classes), dtype=np.float32)
-        for i, goterm in enumerate(unique_go_values):
-            if goterm in funcs_dict[protein_name]:
-                np_ground_truth[i] = 1.0
-        y_test.append(np_ground_truth)
-    train_data = CoevolutionData(x_test, y_test, coev_memmap_dir, coev_ext, pt_seq_dir)
-    # Optional function weighting
-    # function_weights = calculate_label_weights(unique_go_values, funcs_instances_counter, num_proteins)
-    # Preparing index lists for the three main ontologies
-    idxMF = []
-    idxBP = []
-    idxCC = []
-    for i, go_term in enumerate(unique_go_values):
-        if go_term not in list(main_ontologies_per_term_dict.keys()):
-            print("WARNING: Did not find {} in the ontology dictionary, skipping..".format(go_term))
-            continue
-        if main_ontologies_per_term_dict[go_term] == 'F':
-            idxMF.append(i)
-        if main_ontologies_per_term_dict[go_term] == 'P':
-            idxBP.append(i)
-        if main_ontologies_per_term_dict[go_term] == 'C':
-            idxCC.append(i)
-    return (
-        x_test,
-        y_test,
-        train_data,
-        unique_go_values,
-        idxMF,
-        idxBP,
-        idxCC)
 
--- launcher.py
+++ launcher.py
@@ -3,8 +3,6 @@ import sys
 import torch
 import logging
 from data_loader import prepare_data,prepare_test_data
-from trainer import Trainer
-from model import Model
 import numpy as np
 import datetime
 from argparse import ArgumentParser
@@ -17,108 +15,21 @@ sys.path.append(os.path.normpath(os.getcwd() + os.sep + os.pardir))
 
 # Parse arguments
 parser = ArgumentParser()
-parser.add_argument('-g', '--gpu', default=0, dest="gpu", type=int)
 parser.add_argument('-n', '--num_proteins', default=1000, dest="num_proteins", type=int)
-parser.add_argument('-mL', '--max_protein_len', default=1000, dest="max_protein_len", type=int)
-parser.add_argument('-ml', '--min_protein_len', default=1000, dest="min_protein_len", type=int)
-parser.add_argument('-wd', '--weight_decay', default=1e-3, dest="weight_decay", type=float)
-parser.add_argument('-f', '--func_threshold', default=5, dest="func_threshold", type=int)
-parser.add_argument('-e', '--epochs', default=5, dest="epochs", type=int)
-parser.add_argument('-l', '--lr', default=1e-2, dest="learning_rate", type=float)
-parser.add_argument('-o', '--owner', default='', dest='owner', type=str)
-parser.add_argument('-w', '--workers', default=0, dest='workers', type=int)
+
 args = parser.parse_args()
 
 num_loaded_proteins = args.num_proteins   # Number of proteins to load
-max_protein_len = args.max_protein_len
-min_protein_len = args.min_protein_len
-func_threshold = args.func_threshold          # Function count threshold. Encode only functions that cross this threshold
-numWorkers = args.workers              # how many subprocesses to use for data loading
-verbose = True
-load_from_checkpoint = False
-num_epochs = args.epochs
-#max_protein_len = 500  # Coev matrices only computed for this length
-top_n_samples = 5
-train_val_ratio = 0.8
-nr_channels = 461   # input data (co-evolution matrices in form of LxLx441 - L: protein's amino acid sequence length)
-
-# Default arguments for adam optimizer
-adam_lr = args.learning_rate  # learning rate: was 1e-4 before
-adam_betas = (0.9,0.999)   # decay rates for running avg of first and second moments
-adam_eps = 1e-8  # to avoid division by 0
-adam_weight_decay = args.weight_decay  # weight decay
-default_adam_args = {"lr": adam_lr,
-                     "betas": adam_betas,
-                     "eps": adam_eps,
-                     "weight_decay": adam_weight_decay}
-
-
-load_from_ckpt_path = ''
-current_time = datetime.datetime.now().strftime('%Y_%m_%d_%H_%M_%S')
-# Join results_path and loss_path_name to log_path
-results_path = '/usr/prakt/w0184/Desktop/DLproject/result'
-loss_path_name = 'result_' + args.owner + '_%s' % current_time
-log_path = os.path.join(results_path, loss_path_name)
-
-train_loss_file = os.path.join(log_path, 'train_loss')
-loss_avg_file = os.path.join(log_path, 'loss_avg')
-val_loss_file = os.path.join(log_path, 'val_loss')
-val_loss_avg_file = os.path.join(log_path, 'val_loss_avg')
-train_eval_file = os.path.join(log_path, 'train_evaluation')
-val_eval_file = os.path.join(log_path, 'val_evaluation')
-train_gt_and_pred_file = os.path.join(log_path, 'train_gt_and_pred')
-val_gt_and_pred_file = os.path.join(log_path, 'val_gt_and_pred')
-checkpoint_file_path = os.path.join(log_path, 'checkpoint')
-variables_file = os.path.join(log_path, 'variables')
-log_messages_file = os.path.join(log_path, 'messages.log')
-
-log_file_dict = {
-    'log_path': log_path,
-    'train_loss_file': train_loss_file,
-    'loss_avg_file': loss_avg_file,
-    'val_loss_file': val_loss_file,
-    'val_loss_avg_file': val_loss_avg_file,
-    'train_eval_file': train_eval_file,
-    'val_eval_file': val_eval_file,
-    'train_gt_and_pred_file': train_gt_and_pred_file,
-    'val_gt_and_pred_file': val_gt_and_pred_file,
-    'checkpoint_file_path': checkpoint_file_path,
-    'variables': variables_file,
-    'messages': log_messages_file
-}
-if not os.path.isdir(log_file_dict['log_path']):
-    try:
-        os.makedirs(log_file_dict['log_path'])
-    except OSError as exc:  # Guard against race condition
-        raise
-
-logging.basicConfig(filename=log_messages_file, level=logging.DEBUG,
-                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-logger = logging.getLogger(__name__)
-
-print('Log path: {}'.format(log_path))
-
-if not torch.cuda.is_available():
-    print("CUDA is not available, exiting..")
-    quit()
-print('Cuda is available: {}'.format(torch.cuda.is_available()))
-print("Number of GPUs: {}".format(torch.cuda.device_count()))
-device = "cuda:" + str(args.gpu)
-print("Using device '{}'".format(device))
 
 if os.path.isdir('/usr/data/cvpr_shared/biology/function'):
-    data_dir = '/usr/data/cvpr_shared/biology/function/CAFA3/' \
-               'training_data/clustered_70seqid/hhblits_n5_uniclust30_2016_03/'
-    memmap_dir = '/usr/data/cvpr_shared/biology/function/CAFA3/' \
-                 'training_data/clustered_70seqid/hhblits_n5_uniclust30_2016_03/'
-    #added YW
-    pt_seq_dir = '/usr/data/cvpr_shared/biology/function/CAFA3/training_data/clustered_70seqid/fa/'
     test_data_path = '/usr/data/cvpr_shared/biology/function/CAFA3/' \
                  'training_data/clustered_70seqid/hhblits_n5_uniclust30_2016_03/data_protein_pred/test/proteins_holdout_stage2.csv'
     gt_path = '/usr/data/cvpr_shared/biology/function/CAFA3/' \
                  'training_data/clustered_70seqid/hhblits_n5_uniclust30_2016_03/data_protein_pred/test/gt_test_holdout_stage2.npy'
-    # go_term_lookup_path = '/usr/data/cvpr_shared/biology/function/CAFA3/' \
-    #              'training_data/clustered_70seqid/hhblits_n5_uniclust30_2016_03/data_protein_pred/GOtermsLookup_file.csv'
+    go_term_lookup_path = '/usr/data/cvpr_shared/biology/function/CAFA3/' \
+                 'training_data/clustered_70seqid/hhblits_n5_uniclust30_2016_03/data_protein_pred/GOtermsLookup_file.csv'
+    BLAST_KNN_result_path = '/usr/data/cvpr_shared/biology/function/CAFA3/' \
+                            'training_data/clustered_70seqid/hhblits_n5_uniclust30_2016_03/BLAST_KNN/Blast_knn_result_parents.npy'
 else:
     sys.exit('Data directories not found.')
 
@@ -136,18 +47,16 @@ print("Functions in CC: {}".format(len(idxCC)))
 #
 from blast_knn_model import BlastKnnModel
 print("BLAST KNN MODEL")
-path = r"Blast_knn_result_parents.npy"
 
 blastKnnModel = BlastKnnModel(idxMF_test, idxBP_test, idxCC_test,x_test,x_test,unique_go_values_test,y_test,y_test)
-BlastKnnModel.prepare_blast_result(blastKnnModel,path)
+BlastKnnModel.prepare_blast_result(blastKnnModel,BLAST_KNN_result_path)
 # # gotermlookup = pd.read_csv(go_term_lookup_path)
 #
 test_prob,idxMF_all, idxBP_all, idxCC_all = blastKnnModel.blast()
 #np.save("/usr/data/cvpr_shared/biology/function/CAFA3/training_data/clustered_70seqid/hhblits_n5_uniclust30_2016_03/data_protein_pred/Blast_test_result_stage2.npy",test_prob)
 np.save("Blast_test_result_stage2.npy", test_prob)
 # print(np.shape(test_prob))
-go_term_lookup_path = '/usr/data/cvpr_shared/biology/function/CAFA3/' \
-                              'training_data/clustered_70seqid/hhblits_n5_uniclust30_2016_03/data_protein_pred/GOtermsLookup_file.csv'
+
 gotermlookup = pd.read_csv(go_term_lookup_path)
 idxMF_all=np.where(gotermlookup["mainOntology"] == "F")[0]
 #print(np.shape(idxMF_all[0]))
@@ -174,105 +83,5 @@ print("CC:\tF-max: {} Precision: {} Recall: {} Best threshold: {}".format(eval_r
                                                                           eval_results_val['CC']['recall'],
                                                                           eval_results_val['CC']['threshold']))
 print("-----------------------")
-# #added YW
-# from naive_model import NaiveModel
-# print("NAIVE MODEL")
-# naiveModel = NaiveModel(idxMF, idxBP, idxCC,class_weights,y_train,y_val)
-# naiveModel.naive_model()
-# from blast_knn_model import BlastKnnModel
-# print("BLAST KNN MODEL")
-# path = r"Blast_knn_result_parents.npy"
-#
-# blastKnnModel = BlastKnnModel(idxMF, idxBP, idxCC,x_train,x_val,unique_go_values,y_train,y_val)
-# BlastKnnModel.prepare_blast_result(blastKnnModel,path)
-# blastKnnModel.blast()
-# #end added
-# #added YW
-# from trigram_model import TrigramModel
-# print("TRIGRAM MODEL")
-# trigramModel = TrigramModel(idxMF, idxBP, idxCC,x_train,x_val,unique_go_values,y_train,y_val)
-# path = r"protein_trigram.npy"
-# TrigramModel.prepare_trigram_result(trigramModel,path)
-# trigramModel.trigram()
-# path = r"Blast_knn_result.npy"
 
 
-
-#end added
-
-
-
-# Create DataLoader for training data
-train_loader=torch.utils.data.DataLoader(train_data, batch_size=1, shuffle=True, num_workers=numWorkers, pin_memory=True)
-
-# Create DataLoader for validation data
-val_loader=torch.utils.data.DataLoader(val_data, batch_size=1, shuffle=False, num_workers=numWorkers, pin_memory=True)
-
-
-# Load existing or create new model
-if load_from_checkpoint:
-    if verbose:
-        print('Load model from checkpoint')
-    model = torch.load(load_from_ckpt_path)
-else:
-    if verbose:
-        print('Create model')
-    model = Model(num_classes)
-    model.initialize_weights()
-
-# Create new trainer object
-trainer = Trainer(model, device, class_weights, unique_go_values, idxMF, idxBP, idxCC, args_adam=default_adam_args)
-
-# Parameters that do require gradient update
-model_parameters = filter(lambda p: p.requires_grad, model.parameters())
-
-# Print number of parameters that are to be optimized?
-params = sum([np.prod(p.size()) for p in model_parameters])
-if verbose:
-    print('Number of parameters to be optimized: ', params)
-
-with open(variables_file, "a") as f:
-    f.write('log_path:             ' + str(log_path) + '\n' +
-            'checkpoint:           ' + str(load_from_checkpoint) + '\n' +
-            'num_workers:          ' + str(numWorkers) + '\n' +
-            'adam_lr:              ' + str(adam_lr) + '\n' +
-            'adam_betas:           ' + str(adam_betas) + '\n' +
-            'adam_eps:             ' + str(adam_eps) + '\n' +
-            'adam_weight_decay:    ' + str(adam_weight_decay) + '\n' +
-            'num_loaded_proteins:  ' + str(num_loaded_proteins) + '\n' +
-            'train_proteins:       ' + str(len(train_loader.dataset)) + '\n' +
-            'val_proteins:         ' + str(len(val_loader.dataset)) + '\n' +
-            'maxProtLen:           ' + str(max_protein_len) + '\n' +
-            'minProtLen:           ' + str(min_protein_len) + '\n' +
-            '#epochs:              ' + str(num_epochs) + '\n' +
-            'top_n_sampels:        ' + str(top_n_samples) + '\n' +
-            'train_val_r:          ' + str(train_val_ratio) + '\n' +
-            'nr_channels:          ' + str(nr_channels) + '\n' +
-            'func_threshold:       ' + str(func_threshold) + '\n' +
-            'num_classes:          ' + str(num_classes) + '\n' +
-            'unique_GO_terms:      ' + str(unique_go_values) + '\n' +
-            'idxMF                 ' + str(idxMF)+ '\n' +
-            'idxBP                 ' + str(idxBP)+ '\n' +
-            'idxCC                 ' + str(idxCC)+ '\n'
-            )
-    f.close()
-
-# Train model
-if verbose:
-    print('Start training model...')
-trainer.train(train_loader, val_loader, max_protein_len, num_epochs, log_file_dict, verbose)
-
-
-# #added YW
-# from naive_model import NaiveModel
-# print("NAIVE MODEL")
-# naiveModel = NaiveModel(idxMF, idxBP, idxCC,class_weights,y_train,y_val)
-# naiveModel.naive_model()
-# from blast_knn_model import BlastKnnModel
-# print("BLAST KNN MODEL")
-# path = r"Blast_knn_result.npy"
-#
-# blastKnnModel = BlastKnnModel(idxMF, idxBP, idxCC,x_train,x_val,unique_go_values,y_train,y_val)
-# BlastKnnModel.prepare_blast_result(blastKnnModel,path)
-# blastKnnModel.blast()
-# #end added
