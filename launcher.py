5a997a557cd8a2bcb8529480dbfb57efad623589
Wu Yihan <yihan.wu@tum.de>
2019-03-02 01:06:37 +0100

    changes

--- launcher.py
+++ launcher.py
@@ -33,7 +33,7 @@ if os.path.isdir('/usr/data/cvpr_shared/biology/function'):
     go_term_lookup_path = '/usr/data/cvpr_shared/biology/function/CAFA3/' \
                  'training_data/clustered_70seqid/hhblits_n5_uniclust30_2016_03/data_protein_pred/GOtermsLookup_file.csv'
     BLAST_KNN_result_path = '/usr/data/cvpr_shared/biology/function/CAFA3/' \
-                            'training_data/clustered_70seqid/hhblits_n5_uniclust30_2016_03/BLAST_KNN/Blast_knn_result_parents.npy'
+                            'training_data/clustered_70seqid/hhblits_n5_uniclust30_2016_03/BLAST_KNN/data/Blast_knn_result_parents.npy'
 else:
     sys.exit('Data directories not found.')
 
@@ -54,38 +54,7 @@ print("BLAST KNN MODEL")
 
 blastKnnModel = BlastKnnModel(idxMF, idxBP, idxCC,x,unique_go_values,y)
 BlastKnnModel.prepare_blast_result(blastKnnModel,BLAST_KNN_result_path)
-# # gotermlookup = pd.read_csv(go_term_lookup_path)
-#
 test_prob,gt = blastKnnModel.blast()
-#np.save("/usr/data/cvpr_shared/biology/function/CAFA3/training_data/clustered_70seqid/hhblits_n5_uniclust30_2016_03/data_protein_pred/Blast_test_result_stage2.npy",test_prob)
-# np.save("Blast_test_result_stage2.npy", test_prob)
-# # print(np.shape(test_prob))
-#
-# gotermlookup = pd.read_csv(go_term_lookup_path)
-# idxMF_all=np.where(gotermlookup["mainOntology"] == "F")[0]
-# #print(np.shape(idxMF_all[0]))
-# idxBP_all=np.where(gotermlookup["mainOntology"] == "P")[0]
-# idxCC_all=np.where(gotermlookup["mainOntology"] == "C")[0]
-# print("BLAST KNN MODEL ALL")
-# gt_test = np.load(gt_path)
-# #print(np.shape(gt_test))
-# test_prob = np.load("Blast_test_result_stage2.npy")
-#print(np.shape(torch.squeeze(torch.FloatTensor(test_prob))))
-# eval_results_val = evaluate(torch.squeeze(torch.FloatTensor(test_prob)),torch.squeeze( torch.FloatTensor(gt_test)), idxMF_all, idxBP_all, idxCC_all)
-# print("-----------------------")
-# print("VALIDATION RESULTS:")
-# print("MF:\tF-max: {} Precision: {} Recall: {} Best threshold: {}".format(eval_results_val['MF']['f_max'],
-#                                                                           eval_results_val['MF']['precision'],
-#                                                                           eval_results_val['MF']['recall'],
-#                                                                           eval_results_val['MF']['threshold']))
-# print("BP:\tF-max: {} Precision: {} Recall: {} Best threshold: {}".format(eval_results_val['BP']['f_max'],
-#                                                                           eval_results_val['BP']['precision'],
-#                                                                           eval_results_val['BP']['recall'],
-#                                                                           eval_results_val['BP']['threshold']))
-# print("CC:\tF-max: {} Precision: {} Recall: {} Best threshold: {}".format(eval_results_val['CC']['f_max'],
-#                                                                           eval_results_val['CC']['precision'],
-#                                                                           eval_results_val['CC']['recall'],
-#                                                                           eval_results_val['CC']['threshold']))
-# print("-----------------------")
+
 
 
