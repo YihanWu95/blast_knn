596328103a8e4c76fc4f469c54c6e7901d6c4125
winter semester acc#184 <w0184@atcremers64.informatik.tu-muenchen.de>
2019-03-14 15:59:38 +0100

    faster2

--- data_loader.py
+++ data_loader.py
@@ -13,14 +13,15 @@ import scipy.sparse as sp
 
 
 def prepare_data(protein_data_path,num_proteins,go_term_lookup_path,go_term_protein_path):
+    print("loading proteins...")
     proteins = pd.read_csv(protein_data_path)
     protein_test_name = proteins['name'][0:num_proteins]
     selected_rows = proteins[proteins["name"].isin(protein_test_name)]["Unnamed: 0"].tolist()
-
-
+    print("finishing loading...")
+    print("preparing ground truth matrix...")
     gotermlookup = pd.read_csv(go_term_lookup_path)
     unique_go_values = gotermlookup["termID"].tolist()
-    selected_columns = gotermlookup["index"].tolist()
+#    selected_columns = gotermlookup["index"].tolist()
     idxMF = np.where(gotermlookup["mainOntology"] == "F")[0]
     idxBP = np.where(gotermlookup["mainOntology"] == "P")[0]
     idxCC = np.where(gotermlookup["mainOntology"] == "C")[0]
@@ -29,7 +30,9 @@ def prepare_data(protein_data_path,num_proteins,go_term_lookup_path,go_term_prot
     gt = sp.load_npz(go_term_protein_path)
     # groundtruth matrix
     gt_test = gt[selected_rows].todense()
-    gt_test = gt_test[:,selected_columns]
+    print(np.shape(gt_test))
+    print("ground truth matrix is ready!")
+#    gt_test = gt_test[:,selected_columns]
 
 
     return (
